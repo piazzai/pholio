be3c0a89f6a04ee1cbfc46fbb7d8a25ddf7a76ec
piazzai <piazzai@pm.me>
2023-02-24 08:05:13 +0100

    Unbrand scripts

--- scripts/start.js
+++ scripts/start.js
@@ -4,10 +4,10 @@ const upath = require('upath');
 const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_modules/.bin/browser-sync');
 
 const { result } = concurrently([
-    { command: 'node scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
+    { command: 'node scripts/watch.js', name: 'WATCH', prefixColor: 'bgBlue.bold' },
     { 
         command: `"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
-        name: 'SB_BROWSER_SYNC', 
+        name: 'BROWSER_SYNC', 
         prefixColor: 'bgGreen.bold',
     }
 ], {
